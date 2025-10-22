c5155a0b4c2e79e789840614331ca417a0f144b2
unknown <santiago.gualdron-r@labinfo.is.escuelaing.edu.co>
2025-10-22 09:50:19 -0500

    commit por simulacro

--- catalan/src/main/java/com/edu/eci/catalan/Service/CatalanService.java
+++ catalan/src/main/java/com/edu/eci/catalan/Service/CatalanService.java
@@ -2,22 +2,30 @@ package com.edu.eci.catalan.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class CatalanService {
+
+    private List<Integer> catNumbers = new ArrayList<>();
     public List<Integer> getCatalanList(Integer value) {
         if (value<1){
             return null;
         }
-        List<Integer> catNumbers = new ArrayList<>();
-        int newValue;
+        catNumbers.add(1);
         if (value==1){
-            catNumbers.add(value);
             return catNumbers;
         }else {
-            newValue = value;
+            catalanRecurrence(value, 0);
         }
-        while (newValue != 1){
-            
+    }
+
+    public int catalanRecurrence(Integer value, Integer count){
+        if (Objects.equals(count, value)) {
+            return 1;
+        }else {
+            Integer ci = (catNumbers.get(count)*) + ;
+            catNumbers.add(ci);
+            return ci;
         }
     }
 }
