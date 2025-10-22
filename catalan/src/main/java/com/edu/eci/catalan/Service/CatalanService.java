53720dc9c6728cdb5d424fc589b1110c62d997e9
unknown <santiago.gualdron-r@labinfo.is.escuelaing.edu.co>
2025-10-22 10:38:46 -0500

    correccion de la matematica del catalan

--- catalan/src/main/java/com/edu/eci/catalan/Service/CatalanService.java
+++ catalan/src/main/java/com/edu/eci/catalan/Service/CatalanService.java
@@ -2,30 +2,36 @@ package com.edu.eci.catalan.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class CatalanService {
 
     private List<Integer> catNumbers = new ArrayList<>();
+
+
     public List<Integer> getCatalanList(Integer value) {
-        if (value<1){
-            return null;
+        catNumbers.add(0, 1);
+        for (int i = 1; i <= value; i++){
+            catNumbers.add(-1);
         }
-        catNumbers.add(1);
-        if (value==1){
-            return catNumbers;
-        }else {
-            catalanRecurrence(value, 0);
+        catalanRecurrence(value);
+        for (int j = 0; j < value; j++){
+            System.out.println(catNumbers.get(j));
         }
+        return catNumbers.subList(0,value + 1);
     }
 
-    public int catalanRecurrence(Integer value, Integer count){
-        if (Objects.equals(count, value)) {
+    public int catalanRecurrence(int value){
+        if (value == 0) {
             return 1;
-        }else {
-            Integer ci = (catNumbers.get(count)*) + ;
-            catNumbers.add(ci);
-            return ci;
+        } else if (catNumbers.get(value) != -1) {
+            return catNumbers.get(value);
+        }else{
+            int response = 0;
+            for (int i = 0; i < value; i++){
+                response += catalanRecurrence(i) * catalanRecurrence(value - i - 1);
+            }
+            catNumbers.add(value, response);
+            return response;
         }
     }
 }
